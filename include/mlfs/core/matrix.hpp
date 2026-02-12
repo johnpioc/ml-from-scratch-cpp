d04e0caaf8b978738e0cac2c8c5964d29e823279
John Pioc <johnpioc15@gmail.com>
2026-02-12 14:50:09 +1000

    docs: added comments to matrix interface

--- include/mlfs/core/matrix.hpp
+++ include/mlfs/core/matrix.hpp
@@ -7,17 +7,26 @@ namespace core {
 
 class Matrix {
 private:
+    /* The number of rows in the matrix */
     int numRows_;
+
+    /* The number of columns in the matrix */
     int numCols_;
+
+    /* The underlying data inside the matrix */
     std::vector<std::vector<double>> data_;
 
 public:
+    /* Initialises a matrix with a given number of rows and columns with each cell set to 0.0 */
     Matrix(int numRows, int numCols);
 
+    /* Initialises a matrix using a given two-dimensional vector */
     Matrix(std::vector<std::vector<double>>& data);
 
+    /* Retrives the value stored at a given row and column number */
     double get(int r, int c);
 
+    /* Sets the value at a given row and column number */
     void set(int r, int c, double val);
 };
 
---
+++ src/models/LinearRegression.cpp
@@ -0,0 +1,9 @@
+#include <mlfs/core/matrix.hpp>
+#include <mlfs/models/linear_regression.hpp>
+
+using namespace mlfs::models;
+using namespace mlfs::core;
+
+void LinearRegression::fit(Matrix& x, std::vector<double>& y) {
+
+}
