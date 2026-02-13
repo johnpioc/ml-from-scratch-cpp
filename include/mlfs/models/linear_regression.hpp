438741d7bc5e9a3779c027c8ef60b1c3fb7086fa
John Pioc <johnpioc15@gmail.com>
2026-02-13 11:29:17 +1000

    feat(linear regression): fit, predict and evaluate

--- include/mlfs/models/linear_regression.hpp
+++ include/mlfs/models/linear_regression.hpp
@@ -1,20 +1,28 @@
 #pragma once
 
 #include <mlfs/core/matrix.hpp>
-#include <vector>
+#include <mlfs/core/vector.hpp>
 
 namespace mlfs {
 namespace models {
 
 class LinearRegression {
 private:
-    double intercept_;
-    std::vector<double> coefficients_;
+    core::Vector beta_;
 
 public:
-    void fit(core::Matrix& x, std::vector<double>& y);
-    std::vector<double> predict(core::Matrix& x);
-    double evaluate(std::vector<double>& yPred, std::vector<double>& yTrue);
+    /* Takes a given matrix of observation predictors and a given vector of observation responses
+     * and performs ordinary least squares to calculate model intercept and coefficients
+     * */
+    void fit(core::Matrix& x, core::Vector& y);
+
+    /* Takes a given matrix of observation and predicts a vector of responses based on 
+     * the trained intercept and coefficient values
+     * */
+    core::Vector predict(core::Matrix& x);
+
+    /* Takes a given vector of predicted responses and true responses and returns a R^2 value */
+    double evaluate(core::Vector& yPred, core::Vector& yTrue);
 };
 
 }
