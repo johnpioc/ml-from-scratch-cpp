97d44c703cb048eeff164b7c000304d6402d979e
John Pioc <johnpioc15@gmail.com>
2026-02-14 15:03:16 +1000

    feat(logistic-regression): interface

---
+++ src/models/logistic_regression.cpp
@@ -0,0 +1,8 @@
+#include <mlfs/core/matrix.hpp>
+#include <mlfs/core/vector.hpp>
+#include <mlfs/models/logistic_regression.hpp>
+
+using namespace mlfs::core;
+using namespace mlfs::models;
+
+void LogisticRegression::fit(Matrix& x, Vector& y) {}
