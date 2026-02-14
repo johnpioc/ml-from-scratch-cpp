f80b6f42a4bbaf20e6380f3fa315f0db603c2930
John Pioc <johnpioc15@gmail.com>
2026-02-14 16:14:04 +1000

    feat(logistic-regression): fit, predict & evaluate

--- src/models/logistic_regression.cpp
+++ src/models/logistic_regression.cpp
@@ -19,7 +19,34 @@ void LogisticRegression::fit(Matrix& x, Vector& y) {
         double estimate = exponential / (1 + exponential);
         double error = y.get(i) - estimate;
 
-        Vector derivative = currentRow * error;
-        this->beta_ -= derivative * LEARNING_RATE;
+        Vector derivative = currentRow * error * LEARNING_RATE;
+        this->beta_ -= derivative;
     }
+}
+
+Vector LogisticRegression::predict(Matrix& x) {
+    Matrix augmented = x.prependOnes();
+    Vector logit = augmented * this->beta_;
+
+    Vector yPred(logit.numCells);
+
+    // Apply exponential function to logit vector values and get prediction
+    for (int i = 0; i < logit.numCells; i++) {
+        double exponential = std::exp(logit.get(i));
+        double estimate = exponential / (1 + exponential);
+        yPred.set(i, estimate > 0.5 ? 1 : 0);
+    }
+
+    return yPred;
+}
+
+double LogisticRegression::evaluate(Vector& yPred, Vector& yTrue) {
+    int n = yPred.numCells;
+    int numErrors = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (yPred.get(i) != yTrue.get(i)) numErrors++;
+    }
+
+    return (double) numErrors / (double) n;
 }
