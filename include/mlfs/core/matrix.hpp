13e3b7495a398edcd57cc3a9ff4e1672491e542e
John Pioc <johnpioc15@gmail.com>
2026-02-14 14:52:32 +1000

    refactor(matrix): used initializer lists and got rid of useless getters

--- include/mlfs/core/matrix.hpp
+++ include/mlfs/core/matrix.hpp
@@ -8,27 +8,21 @@ namespace core {
 
 class Matrix {
 private:
-    /* The number of rows in the matrix */
-    int numRows_;
-
-    /* The number of columns in the matrix */
-    int numCols_;
-
     /* The underlying data inside the matrix */
     std::vector<std::vector<double>> data_;
 
 public:
+    /* The number of rows in the matrix */
+    int numRows;
+
+    /* The number of columns in the matrix */
+    int numCols;
+
     /* Initialises a matrix with a given number of rows and columns with each cell set to 0.0 */
     Matrix(int numRows, int numCols);
 
     /* Initialises a matrix using a given two-dimensional vector */
     Matrix(std::vector<std::vector<double>>& data);
-
-    /* Retrieves the number of rows this matrix has */
-    int getNumRows();
-
-    /* Retrives the number of cols this matrix has */
-    int getNumCols();
 
     /* Retrives the value stored at a given row and column number */
     double get(int r, int c);
