c95a9d9f9deb4f889aed330b0d892a4cb0b5ba1b
John Pioc <johnpioc15@gmail.com>
2026-02-16 10:25:26 +1000

    feat(tuning): added concept badges for specific models

--- include/mlfs/models/tuning/evaluation_metric.hpp
+++ include/mlfs/models/tuning/evaluation_metric.hpp
@@ -2,13 +2,25 @@
 
 #include <concepts>
 #include <mlfs/core/vector.hpp>
+#include <mlfs/models/tuning/traits.hpp>
 #include <utility>
 
 namespace mlfs::models::tuning {
 
 template <typename T, typename... Args>
-concept LinearRegressionEvaluationMetric = requires(T metric, core::Vector& yPred, 
-    core::Vector& yTrue, Args&&... args) {
+concept EvaluationMetric = requires(T metric, core::Vector& yPred, core::Vector& yTrue, 
+    Args&&... args) {
+    { metric.evaluate(yPred, yTrue, std::forward<Args>(args)...) } -> std::same_as<double>;
+};
+
+// =============================================================================================== 
+// LINEAR REGRESSION EVALUATION METRICS
+// =============================================================================================== 
+//
+template <typename T, typename... Args>
+concept LinearRegressionEvaluationMetric = 
+    EvaluationMetric<T> && forLinearRegression<T> &&
+    requires(T metric, core::Vector& yPred, core::Vector& yTrue, Args&&... args) {
         { metric.evaluate(yPred, yTrue, std::forward<Args>(args)...) } -> std::same_as<double>;
 };
 
@@ -17,4 +29,6 @@ public:
     double evaluate(core::Vector& yPred, core::Vector& yTrue);
 };
 
+template<>
+inline constexpr bool forLinearRegression<RSquared> = true;
 }
--- include/mlfs/models/tuning/fitting_policy.hpp
+++ include/mlfs/models/tuning/fitting_policy.hpp
@@ -2,13 +2,25 @@
 
 #include <mlfs/core/vector.hpp>
 #include <mlfs/core/matrix.hpp>
+#include <mlfs/models/tuning/traits.hpp>
 #include <utility>
 
 namespace mlfs::models::tuning {
 
 template<typename T, typename... Args>
-concept LinearRegressionFittingPolicy = requires(T policy, core::Matrix& x, core::Vector& y, 
+concept FittingPolicy = requires(T policy, core::Matrix& x, core::Vector& y, 
     Args&&... args) {
+    { policy.fit(x, y, std::forward<Args>(args)...) } -> std::same_as<core::Vector>;
+};
+
+// =============================================================================================== 
+// LINEAR REGRESSION FITTING POLICIES
+// =============================================================================================== 
+
+template<typename T, typename... Args>
+concept LinearRegressionFittingPolicy = 
+    FittingPolicy<T> && forLinearRegression<T> &&
+    requires(T policy, core::Matrix& x, core::Vector& y, Args&&... args) {
     { policy.fit(x, y, std::forward<Args>(args)...) } -> std::same_as<core::Vector>;
 };
 
@@ -17,10 +29,16 @@ public:
     core::Vector fit(core::Matrix& x, core::Vector& y);
 };
 
+template<>
+inline constexpr bool forLinearRegression<OLS> = true;
+
 class Ridge {
 public:
     core::Vector fit(core::Matrix& x, core::Vector& y);
     core::Vector fit(core::Matrix& x, core::Vector& y, double lambda);
 };
+
+template<>
+inline constexpr bool forLinearRegression<Ridge> = true;
 
 }
