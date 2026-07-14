fe3c32ce8dc46071eb069609dbe06c99423f3b7a
johnpioc <johnpioc15@gmail.com>
2026-07-14 19:18:13 +1000

    feat(benchmark): added multiple OLS benchmarks

--- benchmarks/ols_bench.cpp
+++ benchmarks/ols_bench.cpp
@@ -10,10 +10,10 @@
 using jmll::core::Matrix;
 using jmll::core::Vector;
 
-static void BENCHMARK_ols(benchmark::State& state) {
+static void ols_10(benchmark::State& state) {
     using jmll::benchmark::data_generation::makeLinearDataset;
     using jmll::models::OLS;
-    std::pair<Matrix, Vector> linearDataset = makeLinearDataset(500, 10);
+    std::pair<Matrix, Vector> linearDataset = makeLinearDataset(10, 10);
 
     Matrix data = linearDataset.first;
     Vector labels = linearDataset.second;
@@ -25,4 +25,36 @@ static void BENCHMARK_ols(benchmark::State& state) {
     }
 }
 
-BENCHMARK(BENCHMARK_ols)->Unit(benchmark::kMillisecond);
+static void ols_100(benchmark::State& state) {
+    using jmll::benchmark::data_generation::makeLinearDataset;
+    using jmll::models::OLS;
+    std::pair<Matrix, Vector> linearDataset = makeLinearDataset(100, 100);
+
+    Matrix data = linearDataset.first;
+    Vector labels = linearDataset.second;
+
+    for (auto _ : state) {
+        OLS model;
+        model.fit(data, labels);
+        benchmark::DoNotOptimize(model);
+    }
+}
+
+static void ols_1000(benchmark::State& state) {
+    using jmll::benchmark::data_generation::makeLinearDataset;
+    using jmll::models::OLS;
+    std::pair<Matrix, Vector> linearDataset = makeLinearDataset(1000, 1000);
+
+    Matrix data = linearDataset.first;
+    Vector labels = linearDataset.second;
+
+    for (auto _ : state) {
+        OLS model;
+        model.fit(data, labels);
+        benchmark::DoNotOptimize(model);
+    }
+}
+
+BENCHMARK(ols_10)->Unit(benchmark::kMillisecond);
+BENCHMARK(ols_100)->Unit(benchmark::kMillisecond);
+BENCHMARK(ols_1000)->Unit(benchmark::kMillisecond);
